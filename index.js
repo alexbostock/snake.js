e4c0694df7ad57e9a8b5c6a980fc0d3e8965512a
alexbostock <alex@alexbostock.co.uk>
2017-07-06 15:11:41 +0100

    Create skeleton application

---
+++ index.js
@@ -0,0 +1,8 @@
+var server = require("diet");
+var app = server();
+app.listen(8000);
+
+var static = require("diet-static")({path : app.path + "/static"});
+
+app.footer(static);
+
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bine/env sh
+# Script to install the project in the current directory
+
+npm install diet
+npm install diet-static
+
